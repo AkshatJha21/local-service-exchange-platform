78f28ce8a3c4edf33d2441f77b06479adf1b2c4b
AkshatJha21 <akshatj2134@gmail.com>
2024-01-22 18:59:19 +0530

    Service rendering DONE

--- app/(main)/(routes)/communities/[communityId]/trades/[tradeId]/page.tsx
+++ app/(main)/(routes)/communities/[communityId]/trades/[tradeId]/page.tsx
@@ -1,21 +1,21 @@
 import { AddServiceButton } from "@/components/service/add-service-btn";
 import { Button } from "@/components/ui/button";
+import Currency from "@/components/ui/currency";
 import { useModal } from "@/hooks/use-modal-store";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { Service } from "@prisma/client";
-import { Plus } from "lucide-react";
 import Image from "next/image";
 
 interface TradeIdProps {
     params: {
         tradeId: string;
-    },
-    service: Service;
+        serviceId: string;
+    }
 }
 
-const TradeIdPage = async ({ service, params }: TradeIdProps) => {
+const TradeIdPage = async ({ params }: TradeIdProps) => {
 
     const profile = await currentProfile();
 
@@ -29,22 +29,34 @@ const TradeIdPage = async ({ service, params }: TradeIdProps) => {
         }
     });
 
+    const services = await db.service.findMany({
+        where: {
+            id: params.serviceId,
+            tradeId: trade?.id
+        }
+    });
+
     return (
         <div className="flex flex-col">
             <h1 className="text-xl font-semibold my-2 ml-4">{trade?.name}</h1>
             <div className="container px-5 py-14 mx-auto">
                 <div className="flex flex-wrap gap-x-4 gap-y-4">
+                
+                {services.map((service) => (
                     <div className="lg:w-[250px] md:w-3/5 p-4 w-full shadow-lg rounded-md border dark:border-zinc-700/20">
-                        <a className="block relative h-40 rounded overflow-hidden">
-                            {/* <Image alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"> */}
-                        </a>
+                        <div className="block relative h-40 rounded overflow-hidden">
+                            <Image fill src={service.imageUrl} alt="service"/>
+                        </div>
                         <div className="mt-4 flex flex-col">
-                            <h2 className="text-primary title-font text-lg font-medium">Responsive Web Design</h2>
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">By John Doe</h3>
-                            <p className="mt-1">$16.00</p>
+                            <h2 className="text-primary title-font text-lg font-medium">{service.name}</h2>
+                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{service.profileId}</h3>
+                            <p className="mt-1">
+                                <Currency value={service?.price}/>
+                            </p>
                             <Button className="mt-2 ml-auto bg-emerald-500 hover:bg-emerald-700">View</Button>
                         </div>
                     </div>
+                ))}
                 </div>
             </div>
             <AddServiceButton />
