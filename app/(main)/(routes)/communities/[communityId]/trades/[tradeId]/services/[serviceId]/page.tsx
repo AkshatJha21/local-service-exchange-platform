67ea50a2b9571d9ac1e0bf647b60e22a269d70f9
AkshatJha21 <akshatj2134@gmail.com>
2024-03-02 18:54:41 +0530

    design for review 2 done

--- app/(main)/(routes)/communities/[communityId]/trades/[tradeId]/services/[serviceId]/page.tsx
+++ app/(main)/(routes)/communities/[communityId]/trades/[tradeId]/services/[serviceId]/page.tsx
@@ -5,10 +5,14 @@ import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { ChevronLeft } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ServiceIdProps {
     params: {
+        tradeId: string;
         serviceId: string;
+        profileId: string;
+        communityId: string;
     }
 }
 
@@ -20,40 +24,59 @@ const ServiceIdPage = async ({ params }: ServiceIdProps) => {
         return redirectToSignIn();
     }
 
+    const trade = await db.trade.findUnique({
+        where: {
+            id: params.tradeId,
+        }
+    });
+
     const service = await db.service.findUnique({
         where: {
             id: params.serviceId,
         }
     })
 
+    const img = service?.imageUrl;
+
     return ( 
         <div>
             <Button className="dark:hover:bg-zinc-700 text-center m-2 transition" variant="ghost">
                 <ChevronLeft className="h-4 w-4 mr-2"/>
-                Back
+                <Link href={`/communities/${params?.communityId}/trades/${trade?.id}`}>
+                    Back
+                </Link>
             </Button>
             {/* Header */}
-            <div>
-                <h1 className="text-xl font-semibold my-2 ml-4">{service?.name}</h1>
+            <div className="my-2 ml-4">
+                <h1 className="text-xl font-semibold">{service?.name}</h1>
                 <p>Offered by {profile.name}</p>
             </div>
             {/* Service Details */}
-            <div>
-                <h1 className="text-xl font-medium">Description</h1>
-                <p>{service?.details}</p>
-                <h1>Price</h1>
-                <Currency value={service?.price}/>
-            </div>
-            {/* Service Image */}
-            <div>
-                <Image height={240} width={240} src={service?.imageUrl} alt="service"/>
+            <div className="my-6 ml-4 flex justify-between">
+                <div>
+                    <h1 className="text-md font-medium mb-2">Service Details</h1>
+                    <p>{service?.details}</p>
+                    <h1 className="text-md font-medium mt-2">Price</h1>
+                    <Currency value={service?.price}/>
+                </div>
+            {/* Service Image */}   
+                <div className="m-4">
+                    <Image height={350} width={350} src={img} alt="service"/>
+                </div>
             </div>
             {/* Bottom Hire bar */}
-            <div>
-                <h1 className="font-semibold my-2 ml-4">{service?.name}</h1>
-                <p>By {profile.name}</p>
-                <p>at <Currency value={service?.price}/></p>
-                <Button>Hire Now</Button>
+            <div className="absolute bottom-2 m-4 w-8/12">
+                <hr className="w-full bg-slate-400 mb-2"/> 
+                <div className="flex justify-between">
+                    <div>
+                        <h1 className="font-semibold text-xl">{service?.name}</h1>
+                        <p className="text-sm">By {profile.name}</p>
+                        <p className="flex gap-x-2">at <Currency value={service?.price}/></p>
+                    </div>
+                    <Button className="my-auto bg-emerald-500 hover:bg-emerald-700">
+                        Hire Now
+                    </Button>
+                </div>
             </div>
         </div>
      );
